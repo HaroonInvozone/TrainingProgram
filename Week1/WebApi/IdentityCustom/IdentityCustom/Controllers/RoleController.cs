8811b7ff4f93da47812b2cf52977257733af361f
haroonInvozone <haroon.ali@invozone.com>
2022-09-12 17:49:37 +0500

    add authenticatioin and authorization

---
+++ Week1/WebApi/IdentityCustom/IdentityCustom/Controllers/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using IdentityCustom.Constant;
+namespace IdentityCustom.Controllers
+{
+    public class RoleController : Controller
+    {
+        [Authorize(Policy = "EmployeeOnly")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //[Authorize(Roles = "Administrator, Manager")]
+        [Authorize(Roles = $"{Constant.Constants.Roles.Administrator},{Constant.Constants.Roles.Manager}")]
+        public IActionResult Manager()
+        {
+            return View();
+        }
+        //[Authorize(Policy = "RequireAdmin")]
+        [Authorize(Roles = $"{Constant.Constants.Roles.Administrator}")]
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}

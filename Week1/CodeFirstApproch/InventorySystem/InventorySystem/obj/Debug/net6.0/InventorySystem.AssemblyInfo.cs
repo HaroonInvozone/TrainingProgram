62cc0b281dae52d059d4a19c74c315df4c7434d0
haroonInvozone <haroon.ali@invozone.com>
2022-09-21 11:39:04 +0500

    docker working

--- Week1/CodeFirstApproch/InventorySystem/InventorySystem/obj/Debug/net6.0/InventorySystem.AssemblyInfo.cs
+++ Week1/CodeFirstApproch/InventorySystem/InventorySystem/obj/Debug/net6.0/InventorySystem.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0bd77e86-a63d-47c8-b3a8-983bc1b88089")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
